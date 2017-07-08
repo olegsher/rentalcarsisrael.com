873ae1f92bfad65f67bdaea676d58dd3f7bd563a
Oleg Sher <github@sher.biz>
2017-07-08 22:45:58 +0300

    rentalcarsisrael.com

--- ru/Netanya.php
+++ ru/Netanya.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="ru">
 <head>
-<title>Недорогая аренда прокат автомобиля в Израиле | rentalcarsisrael.com</title>
+<title>Недорогая аренда прокат автомобиля в Нетании | rentalcarsisrael.com</title>
 <meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
 <meta  name="description"  content="аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны 7-8-9 мест" />
 <meta property="fb:app_id" content="1322511981092474" />
@@ -12,14 +12,12 @@
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:width" content="640" />
 <meta property="og:image:height" content="320" />
-<meta property="og:title" content="Аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны" />
+<meta property="og:title" content="Недорогая аренда прокат автомобиля в Нетании, эконом класс и семейные машины, микроавтобусы и минивэны" />
 <meta property="og:description" content="Лучшие цены, нет предоплаты, заказ онлайн, Whatsapp/Viber +972-58-7710101"/>
 <?php include("__header.html"); ?>
 </head>
 <body>
-<?php include("../google"); ?>
-        <?php include("../shema.html"); ?> 
-        <?php include("../yandex.html"); ?>
+<?php include("../seo_stat"); ?>
 
 <?php include("__top.html"); ?>
 
