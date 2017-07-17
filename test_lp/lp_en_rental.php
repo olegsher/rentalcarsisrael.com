52778ef6467ec9b2bb0bf47365d0664381a4315a
Oleg Sher <github@sher.biz>
2017-07-17 18:35:52 +0300

    rentalcarsisrael.com

--- test_lp/lp_en_rental.php
+++ test_lp/lp_en_rental.php
@@ -59,20 +59,19 @@ header('Location: http://www.rentalcarsisrael.com');
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 	
-	<title>Аренда прокат автомобилей в Израиле | rentalcarsisrael.com</title>
-        <meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, 
-      arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, 
-      сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
-        <meta  name="description"  content="Аренда прокат автомобиля в Израиле начиная с 10$" />
+	<title>Rent cars Israel | rentalcarsisrael.com</title>
+        <meta name="keywords" content="" />
+        <meta  name="description"  content="" />
         <meta property="fb:app_id" content="1322511981092474" />
         <meta property="og:type" content="website" />
-        <meta property="og:locale" content="ru_RU" />       
-        <meta property="og:url" content="http://www.rentalcarsisrael.com/test_lp/lp_rus_1.php" />
-        <meta property="og:image" content="http://www.rentalcarsisrael.com/images/car_key_vastama.jpeg"/><meta property="og:image:type" content="image/png" />
+        <meta property="og:locale" content="en" />       
+        <meta property="og:url" content="http://www.rentalcarsisrael.com/test_lp/lp_en_rental.php" />
+        <meta property="og:image" content="http://www.rentalcarsisrael.com/images/car_key_vastama.jpeg"/>
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="640" />
         <meta property="og:image:height" content="320" />
-        <meta property="og:title" content="Аренда прокат автомобиля в Израиле начиная с 10$. Бронируйте +972-58-7710101"/>
-        <meta property="og:description" content="аренда прокат от Hyundai i10 и до BMW ן535 в Израиле, аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны 7-8-9 мест, машины Люкс класса"/>
+        <meta property="og:title" content=""/>
+        <meta property="og:description" content=""/>
   
 
 	<link type="image/x-icon" href="img/favicon.ico" rel="shortcut icon">
