e9e9a9bbf8635129f316dae88bcfa4d3dd4c746f
Oleg Sher <github@sher.biz>
2019-12-20 00:52:17 +0200

    removed zadarma

--- ru/benGurion.php
+++ ru/benGurion.php
@@ -102,20 +102,6 @@
 
 <?php include("__footer.html"); ?>
 
-<script type="text/javascript">
-var ZCallbackWidgetLinkId  = '86c302f105eb53a8f2c43e54f6be69ad';
-var ZCallbackWidgetDomain  = 'my.zadarma.com';
-(function(){
-    var lt = document.createElement('script');
-    lt.type ='text/javascript';
-    lt.charset = 'utf-8';
-    lt.async = true;
-    lt.src = 'https://' + ZCallbackWidgetDomain + '/callbackWidget/js/main.min.js?unq='+Math.floor(Math.random(0,1000)*1000);
-    var sc = document.getElementsByTagName('script')[0];
-    if (sc) sc.parentNode.insertBefore(lt, sc);
-    else document.documentElement.firstChild.appendChild(lt);
-})();
-</script>
 
 
 </body>
