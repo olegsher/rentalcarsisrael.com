7942fd7222ecaa13c36fbce8ad0e25e3b9734327
Oleg Sher <github@sher.biz>
2020-01-04 03:08:20 +0200

    initial

--- rentalcarsisrael.py
+++ rentalcarsisrael.py
@@ -9,8 +9,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    #"MAIL_USERNAME": os.environ['EMAIL_USER'],
-    #"MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
 }
 
 app.config.update(mail_settings)
